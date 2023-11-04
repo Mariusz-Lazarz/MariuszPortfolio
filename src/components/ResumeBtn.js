2b736a77a24433a841ddd6a6a1f02bbe43685866
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-11-04 10:23:10 +0100

    chaning download path file

--- src/components/ResumeBtn.js
+++ src/components/ResumeBtn.js
@@ -2,7 +2,7 @@ import styles from "./ResumeBtn.module.css";
 
 const ResumeBtn = () => {
   return (
-    <a href="src\cv\Mariusz CV-1.pdf" download className={styles.btnGradAnchor}>
+    <a href="src/cv/MariuszCV.pdf" download className={styles.btnGradAnchor}>
       <button className={styles.btnGrad}>Get Resume</button>
     </a>
   );
