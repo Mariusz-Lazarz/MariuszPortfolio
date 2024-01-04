f5376f57ba4e82d40214fd14d327f257ca9e5547
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2024-01-04 10:02:37 +0100

    adding new projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,8 @@
 import styles from "./Work.module.css";
 import project1 from "../img/project1.png";
 import project2 from "../img/project2.png";
+import project3 from "../img/project3.webp";
+import project4 from "../img/project4.jpg";
 
 const Work = () => {
   return (
@@ -31,6 +33,7 @@ const Work = () => {
           <div className={styles.workSkills}>
             <button className={styles.glassButton}>React</button>
             <button className={styles.glassButton}>Redux</button>
+            <button className={styles.glassButton}>TailwindCSS</button>
             <button className={styles.glassButton}>Fiebase</button>
             <button className={styles.glassButton}>Netlify</button>
           </div>
@@ -44,6 +47,88 @@ const Work = () => {
             </a>
             <a
               href="https://github.com/Mariusz-Lazarz/Storly"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.codeBtn}>Code</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.container} ${styles.reverse}`}>
+        <div className={styles.projectDetails}>
+          <div className={styles.softCircle}></div>
+          <h1 className={styles.projectTitle}>API for SWAPI</h1>
+          <p className={styles.projectDesc}>
+            A NodeJs API, designed for a StarWars-themed application, utilizes
+            Express.js. Its primary function is to retrieve information from
+            SWAPI, incorporating a 24-hour caching mechanism via MongoDB.
+            Comprehensive documentation is provided through Swagger, and the
+            entire application undergoes testing with JEST.
+          </p>
+          <div className={styles.workSkills}>
+            <button className={styles.glassButton}>NodeJS</button>
+            <button className={styles.glassButton}>ExpresJS</button>
+            <button className={styles.glassButton}>MongoDB</button>
+            <button className={styles.glassButton}>JEST</button>
+            <button className={styles.glassButton}>Docker</button>
+          </div>
+          <div className={styles.workLinks}>
+            {/* <a
+              href="https://mariusz-lazarz.github.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.liveBtn}>Live</button>
+            </a> */}
+            <a
+              href="https://github.com/Mariusz-Lazarz/nodeapi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.codeBtn}>Code</button>
+            </a>
+          </div>
+        </div>
+        <div className={styles.projectImage}>
+          <img src={project3} alt="API for SWAPI" className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.projectImage}>
+          <img
+            src={project4}
+            alt="LolEsports Scraping Bot"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.projectDetails}>
+          <div className={styles.softCircle}></div>
+          <h1 className={styles.projectTitle}>LolEsports Scraping Bot</h1>
+          <p className={styles.projectDesc}>
+            A bot designed for scraping data regarding League of Legends
+            matches. The entire application is hosted on an AWS S3 Bucket and
+            makes use of Lambda functions for scheduling periodic data scraping
+            tasks. After scraping, the data is stored in a Realtime database
+            using Firebase.
+          </p>
+          <div className={styles.workSkills}>
+            <button className={styles.glassButton}>NodeJS</button>
+            <button className={styles.glassButton}>Puppeteer</button>
+            <button className={styles.glassButton}>AWS S3</button>
+            <button className={styles.glassButton}>AWS Lambda</button>
+            <button className={styles.glassButton}>Fiebase</button>
+          </div>
+          <div className={styles.workLinks}>
+            {/* <a
+              href="https://storly.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.liveBtn}>Live</button>
+            </a> */}
+            <a
+              href="https://github.com/Mariusz-Lazarz/LolScraping"
               target="_blank"
               rel="noopener noreferrer"
             >
