e6bacf988e075a18a2b357f3ed19cb7989ce6cdd
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2024-01-17 16:05:04 +0100

    adding parallax effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,48 @@
 import styles from "./About.module.css";
 import profileImage from "../img/me.jpg";
 import Skills from "./Skills";
+import { Parallax } from "react-scroll-parallax";
 
 const About = () => {
   return (
     <section className={styles.section}>
       <h1 className={styles.skillsTitle}>
         Discover my skills below, and for more, see my{" "}
-        <a href="/mariuszcv.pdf" download>resume.</a>
+        <a href="/mariuszcv.pdf" download>
+          resume.
+        </a>
       </h1>
       <div className={styles.container}>
-        <div className={styles.info}>
-          <div className={styles.softCircle}></div>
-          <p className={styles.about}>
-            Proactive Technical Support Engineer boasting 1-2 years of handson
-            experience in the technological domain. I bring to the table
-            proficiency in JavaScript, API integration, and troubleshooting in
-            web development environments. My skill set extends to web analytics,
-            where I shine in deciphering and amplifying digital processes to
-            enhance user experiences and drive operational efficiencies. Eager
-            to pivot into a development role, I bring a reputation for rapid
-            learning, potent communication, and a collaborative ethos that
-            enhances team dynamics and project outcomes. With a steadfast
-            commitment to technical excellence, I am poised to contribute
-            effectively to web development teams and projects
-          </p>
-        </div>
-        <div>
-          <div className={styles.profileImgWrapper}>
-            <img
-              className={styles.profileImg}
-              src={profileImage}
-              alt="Profile"
-            />
+        <Parallax speed={-5}>
+          <div className={styles.info}>
+            <div className={styles.softCircle}></div>
+            <p className={styles.about}>
+              Proactive Technical Support Engineer boasting 1-2 years of handson
+              experience in the technological domain. I bring to the table
+              proficiency in JavaScript, API integration, and troubleshooting in
+              web development environments. My skill set extends to web
+              analytics, where I shine in deciphering and amplifying digital
+              processes to enhance user experiences and drive operational
+              efficiencies. Eager to pivot into a development role, I bring a
+              reputation for rapid learning, potent communication, and a
+              collaborative ethos that enhances team dynamics and project
+              outcomes. With a steadfast commitment to technical excellence, I
+              am poised to contribute effectively to web development teams and
+              projects
+            </p>
           </div>
-        </div>
+        </Parallax>
+        <Parallax speed={10}>
+          <div>
+            <div className={styles.profileImgWrapper}>
+              <img
+                className={styles.profileImg}
+                src={profileImage}
+                alt="Profile"
+              />
+            </div>
+          </div>
+        </Parallax>
       </div>
 
       <Skills />
--- src/components/Work.js
+++ src/components/Work.js
@@ -3,6 +3,7 @@ import project1 from "../img/project1.png";
 import project2 from "../img/project2.png";
 import project3 from "../img/project3.webp";
 import project4 from "../img/project4.jpg";
+import { Parallax } from "react-scroll-parallax";
 
 const Work = () => {
   return (
@@ -19,158 +20,175 @@ const Work = () => {
         </a>
       </h1>
       <div className={styles.container}>
-        <div className={styles.projectImage}>
-          <img src={project1} alt="Storly" className={styles.image} />
-        </div>
-        <div className={styles.projectDetails}>
-          <div className={styles.softCircle}></div>
-          <h1 className={styles.projectTitle}>Storly</h1>
-          <p className={styles.projectDesc}>
-            Storly is a feature-rich e-commerce platform that brings a seamless
-            shopping experience to both buyers and sellers. Explore a world of
-            products at your fingertips, or set up your own store with ease.
-          </p>
-          <div className={styles.workSkills}>
-            <button className={styles.glassButton}>React</button>
-            <button className={styles.glassButton}>Redux</button>
-            <button className={styles.glassButton}>TailwindCSS</button>
-            <button className={styles.glassButton}>Fiebase</button>
-            <button className={styles.glassButton}>Netlify</button>
+        <Parallax speed={10}>
+          <div className={styles.projectImage}>
+            <img src={project1} alt="Storly" className={styles.image} />
           </div>
-          <div className={styles.workLinks}>
-            <a
-              href="https://storly.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.liveBtn}>Live</button>
-            </a>
-            <a
-              href="https://github.com/Mariusz-Lazarz/Storly"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.codeBtn}>Code</button>
-            </a>
+        </Parallax>
+        <Parallax speed={-10}>
+          <div className={styles.projectDetails}>
+            <div className={styles.softCircle}></div>
+            <h1 className={styles.projectTitle}>Storly</h1>
+            <p className={styles.projectDesc}>
+              Storly is a feature-rich e-commerce platform that brings a
+              seamless shopping experience to both buyers and sellers. Explore a
+              world of products at your fingertips, or set up your own store
+              with ease.
+            </p>
+            <div className={styles.workSkills}>
+              <button className={styles.glassButton}>React</button>
+              <button className={styles.glassButton}>Redux</button>
+              <button className={styles.glassButton}>TailwindCSS</button>
+              <button className={styles.glassButton}>Fiebase</button>
+              <button className={styles.glassButton}>Netlify</button>
+            </div>
+            <div className={styles.workLinks}>
+              <a
+                href="https://storly.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.liveBtn}>Live</button>
+              </a>
+              <a
+                href="https://github.com/Mariusz-Lazarz/Storly"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.codeBtn}>Code</button>
+              </a>
+            </div>
           </div>
-        </div>
+        </Parallax>
       </div>
       <div className={`${styles.container} ${styles.reverse}`}>
-        <div className={styles.projectDetails}>
-          <div className={styles.softCircle}></div>
-          <h1 className={styles.projectTitle}>API for SWAPI</h1>
-          <p className={styles.projectDesc}>
-            A NodeJs API, designed for a StarWars-themed application, utilizes
-            Express.js. Its primary function is to retrieve information from
-            SWAPI, incorporating a 24-hour caching mechanism via MongoDB.
-            Comprehensive documentation is provided through Swagger, and the
-            entire application undergoes testing with JEST.
-          </p>
-          <div className={styles.workSkills}>
-            <button className={styles.glassButton}>NodeJS</button>
-            <button className={styles.glassButton}>ExpresJS</button>
-            <button className={styles.glassButton}>MongoDB</button>
-            <button className={styles.glassButton}>JEST</button>
-            <button className={styles.glassButton}>Docker</button>
-          </div>
-          <div className={styles.workLinks}>
-            {/* <a
+        <Parallax speed={-10}>
+          <div className={styles.projectDetails}>
+            <div className={styles.softCircle}></div>
+            <h1 className={styles.projectTitle}>API for SWAPI</h1>
+            <p className={styles.projectDesc}>
+              A NodeJs API, designed for a StarWars-themed application, utilizes
+              Express.js. Its primary function is to retrieve information from
+              SWAPI, incorporating a 24-hour caching mechanism via MongoDB.
+              Comprehensive documentation is provided through Swagger, and the
+              entire application undergoes testing with JEST.
+            </p>
+            <div className={styles.workSkills}>
+              <button className={styles.glassButton}>NodeJS</button>
+              <button className={styles.glassButton}>ExpresJS</button>
+              <button className={styles.glassButton}>MongoDB</button>
+              <button className={styles.glassButton}>JEST</button>
+              <button className={styles.glassButton}>Docker</button>
+            </div>
+            <div className={styles.workLinks}>
+              {/* <a
               href="https://mariusz-lazarz.github.io/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <button className={styles.liveBtn}>Live</button>
             </a> */}
-            <a
-              href="https://github.com/Mariusz-Lazarz/nodeapi"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.codeBtn}>Code</button>
-            </a>
+              <a
+                href="https://github.com/Mariusz-Lazarz/nodeapi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.codeBtn}>Code</button>
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={styles.projectImage}>
-          <img src={project3} alt="API for SWAPI" className={styles.image} />
-        </div>
+        </Parallax>
+        <Parallax speed={10}>
+          <div className={styles.projectImage}>
+            <img src={project3} alt="API for SWAPI" className={styles.image} />
+          </div>
+        </Parallax>
       </div>
       <div className={styles.container}>
-        <div className={styles.projectImage}>
-          <img
-            src={project4}
-            alt="LolEsports Scraping Bot"
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.projectDetails}>
-          <div className={styles.softCircle}></div>
-          <h1 className={styles.projectTitle}>LolEsports Scraping Bot</h1>
-          <p className={styles.projectDesc}>
-            A bot designed for scraping data regarding League of Legends
-            matches. The entire application is hosted on an AWS S3 Bucket and
-            makes use of Lambda functions for scheduling periodic data scraping
-            tasks. After scraping, the data is stored in a Realtime database
-            using Firebase.
-          </p>
-          <div className={styles.workSkills}>
-            <button className={styles.glassButton}>NodeJS</button>
-            <button className={styles.glassButton}>Puppeteer</button>
-            <button className={styles.glassButton}>AWS S3</button>
-            <button className={styles.glassButton}>AWS Lambda</button>
-            <button className={styles.glassButton}>Fiebase</button>
+        <Parallax speed={10}>
+          <div className={styles.projectImage}>
+            <img
+              src={project4}
+              alt="LolEsports Scraping Bot"
+              className={styles.image}
+            />
           </div>
-          <div className={styles.workLinks}>
-            {/* <a
+        </Parallax>
+        <Parallax speed={-10}>
+          <div className={styles.projectDetails}>
+            <div className={styles.softCircle}></div>
+            <h1 className={styles.projectTitle}>LolEsports Scraping Bot</h1>
+            <p className={styles.projectDesc}>
+              A bot designed for scraping data regarding League of Legends
+              matches. The entire application is hosted on an AWS S3 Bucket and
+              makes use of Lambda functions for scheduling periodic data
+              scraping tasks. After scraping, the data is stored in a Realtime
+              database using Firebase.
+            </p>
+            <div className={styles.workSkills}>
+              <button className={styles.glassButton}>NodeJS</button>
+              <button className={styles.glassButton}>Puppeteer</button>
+              <button className={styles.glassButton}>AWS S3</button>
+              <button className={styles.glassButton}>AWS Lambda</button>
+              <button className={styles.glassButton}>Fiebase</button>
+            </div>
+            <div className={styles.workLinks}>
+              {/* <a
               href="https://storly.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <button className={styles.liveBtn}>Live</button>
             </a> */}
-            <a
-              href="https://github.com/Mariusz-Lazarz/LolScraping"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.codeBtn}>Code</button>
-            </a>
+              <a
+                href="https://github.com/Mariusz-Lazarz/LolScraping"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.codeBtn}>Code</button>
+              </a>
+            </div>
           </div>
-        </div>
+        </Parallax>
       </div>
       <div className={`${styles.container} ${styles.reverse}`}>
-        <div className={styles.projectDetails}>
-          <div className={styles.softCircle}></div>
-          <h1 className={styles.projectTitle}>Personal Website</h1>
-          <p className={styles.projectDesc}>
-            This is my initial personal website, featuring a sleek design and
-            attractive animations. It's a highly responsive site that
-            efficiently manages a contact form.
-          </p>
-          <div className={styles.workSkills}>
-            <button className={styles.glassButton}>JavaScript</button>
-            <button className={styles.glassButton}>JQuery</button>
-            <button className={styles.glassButton}>SCSS</button>
+        <Parallax speed={-10}>
+          <div className={styles.projectDetails}>
+            <div className={styles.softCircle}></div>
+            <h1 className={styles.projectTitle}>Personal Website</h1>
+            <p className={styles.projectDesc}>
+              This is my initial personal website, featuring a sleek design and
+              attractive animations. It's a highly responsive site that
+              efficiently manages a contact form.
+            </p>
+            <div className={styles.workSkills}>
+              <button className={styles.glassButton}>JavaScript</button>
+              <button className={styles.glassButton}>JQuery</button>
+              <button className={styles.glassButton}>SCSS</button>
+            </div>
+            <div className={styles.workLinks}>
+              <a
+                href="https://mariusz-lazarz.github.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.liveBtn}>Live</button>
+              </a>
+              <a
+                href="https://github.com/Mariusz-Lazarz/mariusz-lazarz.github.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.codeBtn}>Code</button>
+              </a>
+            </div>
           </div>
-          <div className={styles.workLinks}>
-            <a
-              href="https://mariusz-lazarz.github.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.liveBtn}>Live</button>
-            </a>
-            <a
-              href="https://github.com/Mariusz-Lazarz/mariusz-lazarz.github.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.codeBtn}>Code</button>
-            </a>
+        </Parallax>
+        <Parallax speed={10}>
+          <div className={styles.projectImage}>
+            <img src={project2} alt="Storly" className={styles.image} />
           </div>
-        </div>
-        <div className={styles.projectImage}>
-          <img src={project2} alt="Storly" className={styles.image} />
-        </div>
+        </Parallax>
       </div>
     </section>
   );
